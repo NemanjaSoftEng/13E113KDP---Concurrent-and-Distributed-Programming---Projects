c4504b402c5a0a5c00ccf3e90e9c64f1e3190e9a
NemanjaXXX <nemanjakarapetrovic@gmail.com>
2025-08-31 00:03:08 +0200

    Concepts of processes and synchronization. Locks and barriers. Synchronization algorithms. Different methods of barrier implementations. Semaphores. Distributed binary semaphores, Passing the baton, resource allocation. Monitors. Conditional variables, disciplines for the signal, analysis of monitor state. Message passing. Asynchronous and synchronous message passing. Remote procedure calls.

---
+++ Chat/Chat/src/chat_package/ReadThread.java
@@ -0,0 +1,30 @@
+package chat_package;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class ReadThread extends Thread{
+
+	Socket client;
+	
+	public ReadThread(Socket client) {
+		this.client = client;
+	}
+	
+	public void run() {
+		try (Socket client = this.client; InputStream in = client.getInputStream();
+		BufferedReader pin = new BufferedReader(new InputStreamReader(in));) {
+			
+		String s;
+		while ((s = pin.readLine()) != null) {
+			System.out.println(String.format("%s:%d > %s ",client.getInetAddress(),client.getPort(),s));
+		}
+		
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+		
+}
